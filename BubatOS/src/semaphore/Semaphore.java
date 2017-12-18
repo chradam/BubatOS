15146430e540384f3138a5a2617571dbdc05e953
arturb1922 <32738777+arturb1922@users.noreply.github.com>
2017-12-18 19:54:49 +0100

    Update Semaphore.java

--- BubatOS/src/semaphore/Semaphore.java
+++ BubatOS/src/semaphore/Semaphore.java
@@ -1,5 +1,6 @@
 package semaphore;
-//ogólnie na moje działa, tylko nie ma pełnej funkcjonalnosci bo nie ma planisty. Wątki będą zastąpione przez procesy. Jak coś to pytać, prosić o dodatkowe rzeczy itd.
+//ogólnie na moje działa, tylko nie ma pełnej funkcjonalnosci bo nie ma planisty. Wątki będą zastąpione przez procesy. 
+//Jak coś to pytać, prosić o dodatkowe rzeczy i opierdalać itd.
 import java.util.LinkedList;
 
 public class Semaphore {
